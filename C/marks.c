d8d37faddec4c2e8ab4ae826850da11987dbc5c7
ajeetkumar36697 <75249152+ajeetkumar36697@users.noreply.github.com>
2020-11-30 16:09:18 +0530

    30/11/2020

---
+++ C/marks.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b, c,d;
+	printf("please inter a marks in frist subject;");
+	scanf("%d,$a");
+	printf("please enter a marks in second subject;");
+	scanf("%d,%b");
+	printf("please enter amarks in third subject;");
+	scanf("%d,%c");
+	d=a+b+c;
+	printf("the sum of total marks in three subject%d.",d);
+	
+}
+
